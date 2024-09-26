c6bfcd916fd9888d2323be14d17373c597e7cb98
ali <alinikfarjam344@gmail.com>
2024-09-26 16:26:25 +0330

    Pagination was added for bookingPage

--- hotel/src/features/bookings/BookingTable.jsx
+++ hotel/src/features/bookings/BookingTable.jsx
@@ -4,8 +4,9 @@ import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
 import Spinner from "../../ui/Spinner";
 import { useBookings } from "./useBookings";
+import Pagination from "../../ui/Pagination";
 function BookingTable() {
-  const { bookings, isLoading } = useBookings();
+  const { bookings, isLoading, count } = useBookings();
 
   if (isLoading) return <Spinner />;
   if (!bookings.length) {
@@ -30,6 +31,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination count={count} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
