41eeba4bf2714f461140ced4764dfad217ea9ad3
ali <alinikfarjam344@gmail.com>
2024-09-30 16:43:39 +0330

    dark mode was added

--- hotel/src/features/dashboard/DashboardLayout.jsx
+++ hotel/src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Stattics</div>
+      <div>Stattics</div>
+      <div>Stattics</div>
+      <div>Stattics</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
--- hotel/src/pages/Dashboard.jsx
+++ hotel/src/pages/Dashboard.jsx
@@ -2,16 +2,21 @@ import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import DashboardLayout from "../features/dashboard/DashboardLayout";
+import DashboardFilter from "../features/dashboard/DashboardFilter";
 
 function Dashboard() {
   useEffect(() => {
     getCabins().then((data) => console.log(data));
   }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <DashboardFilter />
+      </Row>
+      <DashboardLayout />
+    </>
   );
 }
 
