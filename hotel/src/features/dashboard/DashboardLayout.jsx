5ce3af5c33c0c248bd986a4843acede48e4df94d
ali <alinikfarjam344@gmail.com>
2024-10-02 16:17:28 +0330

    finish

--- hotel/src/features/dashboard/DashboardLayout.jsx
+++ hotel/src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from "./Stats";
 import { useCabin } from "../cabins/useCabin";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -34,7 +35,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Stattics</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
