27a37cf15cd27111af372ac732732a44d06e0e43
ali <alinikfarjam344@gmail.com>
2024-10-10 13:22:38 +0330

    changes

--- hotel/src/utils/helpers.js
+++ hotel/src/utils/helpers.js
@@ -1,7 +1,7 @@
 import { formatDistance, parseISO } from 'date-fns';
-import dateFnsTz from 'date-fns';
+import { differenceInDays } from 'date-fns';
 
-const { differenceInDays } = dateFnsTz;
+// const  = dateFnsTz;
 // We want to make this function work for both Date objects and strings (which come from Supabase)
 export const subtractDates = (dateStr1, dateStr2) =>
   differenceInDays(parseISO(String(dateStr1)), parseISO(String(dateStr2)));
